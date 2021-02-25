4919a9e12162d0f8a751e3fda32e56646e84a1d0
Jessda <10837509@uvu.edu>
2021-02-24 19:27:50 -0700

    Finished Challenge 2.

--- Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Challenge 2 - Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -9,24 +9,25 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
-
+    private float spawnInterval;
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Invoke("SpawnRandomBall", startDelay);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        int spawnInterval = Random.Range(3,5);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
-
+        int ballPrefabsIndex = Random.Range(0, ballPrefabs.Length);
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballPrefabsIndex], spawnPos, ballPrefabs[ballPrefabsIndex].transform.rotation);
+        Invoke("SpawnRandomBall", spawnInterval);
     }
 
 }
