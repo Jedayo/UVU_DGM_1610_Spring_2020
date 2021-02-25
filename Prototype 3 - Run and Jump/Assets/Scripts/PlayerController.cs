5ca9bc2c0c4b88b8099a517557fbafeccad8d3ae
Jessda <10837509@uvu.edu>
2021-02-24 17:40:57 -0700

    Finished the spawn manager script and implemented boundary control.

--- Prototype 3 - Run and Jump/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - Run and Jump/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool gameOver = false;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier; 
     }
 
@@ -23,11 +26,20 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); 
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         } 
     }
     
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+        }
     }
 }
