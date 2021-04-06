0befc8a2065df30e41c9c06cc88506f2b53a41f2
Jessda <10837509@uvu.edu>
2021-04-06 13:29:33 -0600

    Added player movement and modified camera perspective.

---
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    float zInput;
+    float hInput;
+    public float speed = 15.0f; 
+    public float zRange = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player constraint condition statements
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+
+        // Player input
+        zInput = Input.GetAxis("Vertical");
+        hInput = Input.GetAxis("Horizontal");
+
+        // Player movement
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * zInput * Time.deltaTime * speed);
+    }
+}
